8609151254873505a4ad87deb3ff5d0f6f4767f2
unknown <leif.hammar@gymnasium.ax>
2024-10-29 13:17:30 +0200

    vercel folder

--- shoppinglistor/settings.py
+++ shoppinglistor/settings.py
@@ -129,7 +129,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, "ui/static")]
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "ui/staticfiles")
 
